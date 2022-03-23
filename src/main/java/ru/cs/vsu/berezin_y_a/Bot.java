b6f1092308083f7960f5482decc1f942bf636f09
yaroslavberezin <yb2003work@gmail.com>
2022-03-23 15:17:52 +0300

    V1.01

---
+++ src/main/java/ru/cs/vsu/berezin_y_a/Bot.java
@@ -0,0 +1,157 @@
+package ru.cs.vsu.berezin_y_a;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.methods.send.SendSticker;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Iterator;
+
+public class Bot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        String chatId = update.getMessage().getChatId().toString();
+        String messageText = update.getMessage().getText();
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            if (messageText.contains("/start")) {
+                String stickerId = "CAACAgIAAxkBAAEEO2xiOWxKyxk2V8O5bPWqOf_n-C_hgQACNhYAAlxA2EvbRm7S3ZV6DSME";
+                String text = "Привет! Я не просто бот, а испанский словарь, готовый помочь тебе в трудную минуту!";
+                sendSticker(update, chatId, stickerId);
+                sendMessage(update, chatId, text);
+            } else {
+                String text = "Вот, что мне удалось найти: ";
+                sendMessage(update, chatId, text);
+                sendTranslation(update, chatId, messageText);
+            }
+        }
+    }
+
+    private void sendTranslation(Update update, String chatId, String text) {
+        String api = "N0rZQgmnB7";
+        String request = "http://api.diccionario.ru/?key=[" + api + "]&q=" + text;
+
+        HttpURLConnection connection = null;
+
+        try {
+            connection = (HttpURLConnection) new URL(request).openConnection();
+
+            connection.setRequestMethod("GET");
+            connection.setUseCaches(false);
+            connection.setConnectTimeout(500);
+            connection.setReadTimeout(500);
+
+            connection.connect();
+
+            if (HttpURLConnection.HTTP_OK == connection.getResponseCode()) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                JSONParser jsonParser = new JSONParser();
+
+                JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
+                JSONArray hits = (JSONArray) jsonObject.get("hits");
+
+                Iterator i = hits.iterator();
+                StringBuilder answer = new StringBuilder();
+
+                while (i.hasNext()) {
+                    JSONObject innerObject = (JSONObject) i.next();
+                    String l = (String) innerObject.get("l");
+                    String r = (String) innerObject.get("r");
+
+                    if ((!l.contains("<")) && (!r.contains("<"))) {
+                        answer.append("- ");
+                        answer.append(innerObject.get("l"));
+                        answer.append(" - ");
+                        answer.append(innerObject.get("r"));
+                        answer.append("\n");
+                        answer.append("\n");
+                    } else if (!l.contains("<")) {
+                        answer.append("- ");
+                        answer.append(innerObject.get("l"));
+                        answer.append("\n");
+                        answer.append("\n");
+                    } else if (!r.contains("<")) {
+                        answer.append("- ");
+                        answer.append(innerObject.get("r"));
+                        answer.append("\n");
+                        answer.append("\n");
+                    }
+                }
+
+                sendMessage(update, chatId, answer.toString());
+            } else {
+                System.out.println("fail:" + connection.getResponseCode() + ", " + connection.getResponseMessage());
+            }
+        } catch (
+                Throwable cause) {
+            cause.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+
+    }
+
+    private void sendMessage(Update update, String chatId, String text) {
+        SendMessage message = new SendMessage();
+        message.setChatId(update.getMessage().getChatId().toString());
+        message.setText(text);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Message sent to " + chatId);
+    }
+
+    private void sendImageFromFileId(String fileId, String chatId) {
+        SendPhoto sendPhotoRequest = new SendPhoto();
+        sendPhotoRequest.setChatId(chatId);
+        sendPhotoRequest.setPhoto(new InputFile(fileId));
+
+        try {
+            execute(sendPhotoRequest);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendSticker(Update update, String chatId, String StickerId) {
+        String ChatId = update.getMessage().getChatId().toString();
+        InputFile StickerFile = new InputFile(StickerId);
+        SendSticker TheSticker = new SendSticker(ChatId, StickerFile);
+
+        try {
+            execute(TheSticker);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Sticker sent to " + chatId);
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "Spanish Dictionary RGF Bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "5216972541:AAHp0kqssBOvZhOQr5VjpgCqt5-v-VfNoEA";
+    }
+
+}
---
+++ src/main/java/ru/cs/vsu/berezin_y_a/Main.java
@@ -0,0 +1,22 @@
+package ru.cs.vsu.berezin_y_a;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Bot successfully started!");
+
+    }
+
+}
