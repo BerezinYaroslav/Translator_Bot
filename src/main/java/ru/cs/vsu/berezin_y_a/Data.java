08dc70cb264235a5bb5e9286cc4d14e0fb5ea928
Alexey Brante <96073038+Fextice@users.noreply.github.com>
2022-07-28 02:43:14 +0300

    Update Data.java

--- src/main/java/ru/cs/vsu/berezin_y_a/Data.java
+++ src/main/java/ru/cs/vsu/berezin_y_a/Data.java
@@ -3,6 +3,6 @@ package ru.cs.vsu.berezin_y_a;
 public class Data {
 
     String api = "N0rZQgmnB7";
-    String botToken = "5216972541:AAHp0kqssBOvZhOQr5VjpgCqt5-v-VfNoEA";
+    String botToken = "5216972541:AAHp0kqssBOvZhOQr5VjpgCqt5-v-VfNoEA"; // Мне кажется или хранить токен бота в открытом доступе - плохая затея? :)
 
 }
